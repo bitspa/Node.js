6006855ad2432f3a5bf619d1973dd3219f75b84c
spandita <spandita.banerjee@bitcanny.com>
2023-03-09 17:55:14 +0530

    Till 09th March

--- TemplateEngine.js
+++ TemplateEngine.js
@@ -3,7 +3,7 @@ const app=express();
 app.set("view engine","ejs")
 app.get("/Profile/:name",function(req,res)
 {
-  console.log(req.params.name);
+  data={email:"test@test.com",phoneNumber:8777361605,skills:['node js','php','java','react']}
   res.render('Profile',{name:req.params.name})
 })
 app.listen(5000);
